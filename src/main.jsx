d630b55b8a8b2399ba7a7bb2d5ba1334a6232138
SAJID-AFNAN <sajidafnan91@gmail.com>
2024-01-06 19:06:22 +0600

    Setup navbar and Footer

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Root from './Components/Root/Root'
 import Home from './Components/Home/Home'
+import AppliedJobs from './Components/AppliedJobs/AppliedJobs'
+import Blog from './Components/Blog/Blog'
+import Statistics from './Components/Statistics/Statistics'
 
 const router = createBrowserRouter([
   {
@@ -13,6 +16,18 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>
+      },
+      {
+        path: '/statistics',
+        element: <Statistics></Statistics>
+      },
+      {
+        path: '/applied',
+        element: <AppliedJobs></AppliedJobs>
+      },
+      {
+        path: '/blog',
+        element: <Blog></Blog>
       }
     ]
   }
