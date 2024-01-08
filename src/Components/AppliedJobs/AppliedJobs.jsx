efc6aba63fe2b5391cf01b5e2a53b223b2db8a82
SAJID-AFNAN <sajidafnan91@gmail.com>
2024-01-08 13:29:17 +0600

    Add Filter on Dashboard

--- src/Components/AppliedJobs/AppliedJobs.jsx
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -2,12 +2,27 @@ import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { getStoredJobApplication } from "../../Utility/LocalStorage";
 import SingleDashboard from "../SingleDashboard/SingleDashboard";
+import { IoIosArrowDown } from "react-icons/io";
 
 const AppliedJobs = () => {
     const jobs = useLoaderData();
     const [applied, setApplied] = useState([])
+    const [displayJob, setDisplayJob] = useState([])
 
-    const { logo } = applied
+    const handleFilterJobs = filter => {
+        if(filter === 'All'){
+            setDisplayJob(applied)
+        }
+        else if(filter === 'Remote'){
+            const remoteJobs = applied.filter(job => job.remote_or_onsite === 'Remote')
+            setDisplayJob(remoteJobs)
+        }
+        else if(filter === 'Onsite'){
+            const onsiteJobs = applied.filter(job => job.remote_or_onsite === 'Onsite')
+            setDisplayJob(onsiteJobs)
+        }
+    }
+
 
     useEffect(() => {
         const storedJobs = getStoredJobApplication()
@@ -23,14 +38,23 @@ const AppliedJobs = () => {
             }
             // console.log(jobs, storedJobs, appliedJob);
             setApplied(appliedJob)
+            setDisplayJob(appliedJob)
         }
-    }, [])
+    }, [jobs])
     return (
         <div>
             <h2 className="text-3xl font-extrabold text-center my-10">Jobs Applied</h2>
+            <details className="dropdown">
+                <summary className="m-1 btn">Filter By <IoIosArrowDown></IoIosArrowDown></summary>
+                <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                    <li onClick={() => handleFilterJobs('All')}><a>All</a></li>
+                    <li onClick={() => handleFilterJobs('Remote')}><a>Remote</a></li>
+                    <li onClick={() => handleFilterJobs('Onsite')}><a>Onsite</a></li>
+                </ul>
+            </details>
             <div>
                 {
-                    applied.map(job => <SingleDashboard key={job.id} job={job}></SingleDashboard>)
+                    displayJob.map(job => <SingleDashboard key={job.id} job={job}></SingleDashboard>)
                 }
             </div>
         </div>
--- src/Components/SingleDashboard/SingleDashboard.jsx
+++ src/Components/SingleDashboard/SingleDashboard.jsx
@@ -1,15 +1,29 @@
+import { IoLocationOutline } from "react-icons/io5";
+import { AiOutlineDollar } from "react-icons/ai";
+import { Link } from "react-router-dom";
+
 const SingleDashboard = ({ job }) => {
-    const { logo, job_title, company_name, remote_or_onsite, job_type, location, salary } = job
+    const { id, logo, job_title, company_name, remote_or_onsite, job_type, location, salary } = job
     return (
         <div>
-            <div className="card card-side bg-base-100 shadow-xl">
-                <figure><img className="w-40" src={logo} alt="Movie" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">{job_title}</h2>
-                    <p>Click the button to watch on Jetflix app.</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Watch</button>
+            <div className="card card-side bg-base-100 shadow-xl border border-blue-200 max-w-5xl mx-auto p-5 my-5">
+                <div>
+                    <figure><img className=" w-56 p-16 bg-[#F4F4F4]" src={logo} alt="Movie" /></figure>
+                </div>
+                <div className="px-8">
+                    <div>
+                        <h2 className="card-title">{job_title}</h2>
+                        <p className="my-3 text-[#757575] font-bold">{company_name}</p>
+                        <button className="text-[#9873FF] px-5 py-2 rounded font-extrabold border border-[#9873FF] mr-4">{remote_or_onsite}</button>
+                        <button className="text-[#9873FF] px-5 py-2 rounded font-extrabold border border-[#9873FF] mr-4">{job_type}</button>
+                        <div className="flex mt-3 gap-7 text-[#757575]">
+                            <p className="flex gap-2"><IoLocationOutline className="text-xl"></IoLocationOutline>{location}</p>
+                            <p className="flex gap-2"><AiOutlineDollar className="text-xl"></AiOutlineDollar><span>Salary : </span>{salary}</p>
+                        </div>
                     </div>
+                </div>
+                <div className="card-actions justify-end mx-auto items-center">
+                    <Link to={`/job/${id}`}><button className="btn text-white w-full bg-gradient-to-l from-[#7E90FE] to-[#9873FF]">View Details</button></Link>
                 </div>
             </div>
         </div>
